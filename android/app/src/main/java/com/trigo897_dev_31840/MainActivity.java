9efefaa791063835b9f07c11516ca06e14b846a1
Crowdbotics <engineering@crowdbotics.com>
2022-09-05 09:31:04 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/trigo897_dev_31840/MainActivity.java
@@ -0,0 +1,15 @@
+package com.trigo897_dev_31840;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "trigo897_dev_31840";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "trigo897_dev_31840",
+  url: "https://trigo897_dev_31840.botics.co",
+  api: "https://trigo897_dev_31840.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
